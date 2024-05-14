38ecdc1ea983fefe1746c30e9a94d7349e86be42
APIVerve Team <hello@apiverve.com>
2024-05-14 15:25:46 -0500

    Bug fixes and Improvements

---
+++ duckduckgosearchQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class duckduckgosearchQueryOptions {
+public string query { get; set; }
+}
+
+}
